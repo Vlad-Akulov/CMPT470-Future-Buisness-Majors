b38ee398d2f581d2a5ea1bbe8ca5ce06fc87413d
Vlad Akulov <onc736@usask.ca>
2026-01-20 14:09:24 -0600

    added semgrep instructions and run script

---
+++ assignment2/Part1-Semgrep/run_Semgrep.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Absolute path to this script (resolves symlinks)
+SCRIPT_PATH="$(readlink -f "$0")"
+
+# Parent directory of the script
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+# cd into it
+cd "$SCRIPT_DIR" || exit 1
+
+# load semgrep into path 
+source ./venv/bin/activate
+
+
+# run semgrep on the httpx directory to analyse only the code 
+cd ../httpx/
+rm ../Part1-Semgrep/semgrep.txt
+semgrep --config auto ./httpx/ >> ../Part1-Semgrep/semgrep.txt 2>&1
+
+
