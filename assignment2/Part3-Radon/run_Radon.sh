dd6239b7db8243700860734d1dd22b79b46db0d9
Darian Rowat <ddr271@usask.ca>
2026-01-27 13:29:17 -0600

    Added radon raw

--- assignment2/Part3-Radon/run_Radon.sh
+++ assignment2/Part3-Radon/run_Radon.sh
@@ -23,9 +23,11 @@ cd ../httpx
 
 # outputs (back in Part3-Radon)
 python -m radon cc -s -a ./httpx > ../Part3-Radon/radon_cc.txt 2>&1
-python -m radon mi -s     ./httpx > ../Part3-Radon/radon_mi.txt 2>&1
+python -m radon mi -s ./httpx > ../Part3-Radon/radon_mi.txt 2>&1
+python -m radon raw -s ./httpx > ../Part3-Radon/radon_raw.txt 2>&1
 
 
 echo "Wrote:"
 echo "  ../Part3-Radon/radon_cc.txt"
 echo "  ../Part3-Radon/radon_mi.txt"
+echo "  ../Part3-Radon/radon_raw.txt"
